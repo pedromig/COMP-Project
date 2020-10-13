d88b066485fe3d9ad9c940802777dbe1cd3a71bc
pedromig <pedromiguelrodrigues2000@gmail.com>
2020-10-13 19:19:49 +0100

    Adding some how to use text to the test.sh shell script and some extra funcionalities too

--- src/make.sh
+++ src/make.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 ############# COLORS ###############
-YELLOW="\x1B[33m"
 RED="\x1B[31m"
 GREEN="\x1B[32m"
-ORANGE="\x1B[38;5;208m"
 BLUE="\x1B[34m"
 RESET="\x1B[0m"
 ####################################
+
 LEXER="uccompiler.l"
 COMPILER_NAME="ucc"
 
 if [[ ! -f $LEXER ]]; then
     echo -e "${RED}ERROR:${RESET} $LEXER file does not exist! "
     echo -ne "${BLUE}INFO:${RESET} Creating file... " && touch $LEXER && echo -e "${GREEN}DONE!${RESET}"
+    exit 1;
 fi
 
 if [ $# -eq 1 ]; then
@@ -28,4 +28,5 @@ if [ $? -eq 0 ]; then
     echo -e "${GREEN}DONE!${RESET}"
 else
     echo -e "${RED}FAILED!!${RESET}"
+    exit 1;
 fi
--- src/test.sh
+++ src/test.sh
@@ -15,7 +15,7 @@ function run_tests() {
         ucfile=$(basename "$file_path")
         outfile=${ucfile%.*}.out
 
-        (./$UC_COMPILER $COMPILER_FLAGS <$1/$ucfile) > $2/$outfile
+        ./$UC_COMPILER $COMPILER_FLAGS < $file_path > $2/$outfile
 
         diff $1/$outfile $2/$outfile > /dev/null
 
@@ -25,11 +25,18 @@ function run_tests() {
             echo -e ❌ "${RED}TEST FAILED!!${RESET}" $ucfile
         fi
     done
-
 }
 
-if [[ ! -f $UC_COMPILER ]]; then
-    ./make.sh $UC_COMPILER
+
+if [ $# -eq 0 ]; then
+    echo "Usage: ./test.sh [compiler] [...compiler_args] [...input_dir] [...output_dir]"
+    exit 1;
+else
+    [[ ! -f $UC_COMPILER ]] && ./make.sh $UC_COMPILER
+    
+    [[ $3 ]] && INPUT_DIR=$3
+    [[ $4 ]] && OUTPUT_DIR=$4
+
+    run_tests $INPUT_DIR $OUTPUT_DIR
 fi
 
-run_tests $INPUT_DIR $OUTPUT_DIR
