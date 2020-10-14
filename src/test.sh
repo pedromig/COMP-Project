3c51a2eee3fbb2f84cfdc015b17457d7139ab4d1
pedromig <pedromiguelrodrigues2000@gmail.com>
2020-10-14 10:58:20 +0100

    Making some changes

--- src/test.sh
+++ src/test.sh
@@ -7,17 +7,14 @@ RESET="\x1B[0m"
 INPUT_DIR="../tests/input"
 OUTPUT_DIR="../tests/output"
 
-UC_COMPILER=$1
-COMPILER_FLAGS=$2
-
 function run_tests() {
     for file_path in $1/*.{uc,c}; do
         ucfile=$(basename "$file_path")
         outfile=${ucfile%.*}.out
 
-        ./$UC_COMPILER $COMPILER_FLAGS < $file_path > $2/$outfile
+        ./$UC_COMPILER $COMPILER_FLAGS <$file_path >$2/$outfile
 
-        diff $1/$outfile $2/$outfile > /dev/null
+        diff $1/$outfile $2/$outfile >/dev/null
 
         if [ $? -eq 0 ]; then
             echo -e ✅ "${GREEN}TEST PASSED!!${RESET}" $ucfile
@@ -27,16 +24,28 @@ function run_tests() {
     done
 }
 
-
 if [ $# -eq 0 ]; then
-    echo "Usage: ./test.sh [compiler] [...compiler_args] [...input_dir] [...output_dir]"
-    exit 1;
+    echo "Usage: ./test.sh [compiler] [-args=compiler_args] [-i=input_dir] [-o=output_dir]"
+    exit 1
 else
+    UC_COMPILER=$1
+    shift
+    for option in "$@"; do
+        case $option in
+        -args=* | --compiler-arguments=*)
+            COMPILER_FLAGS="${option#*=}"
+            shift
+            ;;
+        -i=* | --input-dir=*)
+            INPUT_DIR="${option#*=}"
+            shift
+            ;;
+        -o=* | --output-dir=*)
+            OUTPUT_DIR="${option#*=}"
+            shift
+            ;;
+        esac
+    done
     [[ ! -f $UC_COMPILER ]] && ./make.sh $UC_COMPILER
-    
-    [[ $3 ]] && INPUT_DIR=$3
-    [[ $4 ]] && OUTPUT_DIR=$4
-
     run_tests $INPUT_DIR $OUTPUT_DIR
 fi
-
